b7a124f2759b59cc6470e8d2e9407b312c573099
kritzcreek <christoph.hegemann1337@gmail.com>
2023-08-03 14:25:54 +0200

    sets up FreeMarker templating

--- src/main/kotlin/com/serameracorp/plugins/Templating.kt
+++ src/main/kotlin/com/serameracorp/plugins/Templating.kt
@@ -1,20 +1,13 @@
 package com.serameracorp.plugins
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
-    routing {
-        get("/html-freemarker") {
-            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
-        }
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 }
-
-data class IndexData(val items: List<Int>)
