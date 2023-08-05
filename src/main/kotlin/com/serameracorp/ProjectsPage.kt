3d6ee257c25f4234fe24392706bed2dacf2ddecc
kritzcreek <christoph.hegemann1337@gmail.com>
2023-08-06 00:47:01 +0200

    adds a single project page at /project/{projectId}

--- src/main/kotlin/com/serameracorp/ProjectsPage.kt
+++ src/main/kotlin/com/serameracorp/ProjectsPage.kt
@@ -1,19 +1,22 @@
 package com.serameracorp
 
 import com.serameracorp.plugins.connectToPostgres
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.sql.ResultSet
 
-data class Project(val name: String, val pattern: String, val user: String, val imgUrl: String)
+data class Project(val id: Int, val name: String, val pattern: String, val user: String, val imgUrl: String)
 
 fun Route.projects() {
 
   val dbConnection = application.connectToPostgres(embedded = false)
-  val statement = dbConnection.prepareStatement(
+  val allProjectsStatement = dbConnection.prepareStatement(
     """
     | select
+    |   project.id as id,
     |   project.name as name,
     |   pattern.name as pattern,
     |   app_user.username as user 
@@ -23,21 +26,55 @@ fun Route.projects() {
     """.trimMargin()
   )
 
-    val resultSet = statement.executeQuery()
+  val projectByIdStatement = dbConnection.prepareStatement(
+    """
+    | select
+    |   project.id as id,
+    |   project.name as name,
+    |   pattern.name as pattern,
+    |   app_user.username as user 
+    | from project 
+    | join pattern on project.pattern_id = pattern.id 
+    | join app_user on project.app_user_id = app_user.id 
+    | where project.id = ?
+    """.trimMargin()
+  )
+
+  fun projectFromResultSet(resultSet: ResultSet): Project =
+    Project(
+      resultSet.getInt("id"),
+      resultSet.getString("name"),
+      resultSet.getString("pattern"),
+      resultSet.getString("user"),
+      "https://clipart-library.com/newhp/kissclipart-white-clothing-black-dress-line-art-96933c1b56d3f716.png"
+    )
+
   get("/project") {
+    val resultSet = allProjectsStatement.executeQuery()
     val projects = sequence {
       while (resultSet.next()) {
-        val project = Project(
-          resultSet.getString("name"),
-          resultSet.getString("pattern"),
-          resultSet.getString("user"),
-          "https://clipart-library.com/newhp/kissclipart-white-clothing-black-dress-line-art-96933c1b56d3f716.png"
-        )
-        yield(project)
+        yield(projectFromResultSet(resultSet))
       }
     }.toList()
 
     val res = FreeMarkerContent("projects.ftl", mapOf("projects" to projects))
     call.respond(res)
+  }
+
+  get("/project/{projectId}") {
+    val projectId = call.parameters["projectId"]?.toIntOrNull()
+    if (projectId == null) {
+      return@get call.respond(HttpStatusCode.NotFound)
+    }
+
+    projectByIdStatement.setInt(1, projectId)
+    val resultSet = projectByIdStatement.executeQuery()
+    if (resultSet.next()) {
+      val project = projectFromResultSet(resultSet)
+      val res = FreeMarkerContent("project.ftl", mapOf("project" to project))
+      call.respond(res)
+    } else {
+      call.respond(HttpStatusCode.NotFound)
+    }
   }
 }
---
+++ src/main/resources/templates/project.ftl
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Static page</title>
+    <link rel="stylesheet" href="/static/main.css">
+</head>
+<body>
+<div class="project">
+    <img src="${project.imgUrl}" alt="" class="project-img">
+    <a href="" class="project-name">
+        ${project.name}
+    </a>
+    <a href="" class="project-user">
+        ${project.user}
+    </a>
+    <a href="" class="project-pattern">
+        ${project.pattern}
+    </a>
+</div>
+</body>
+</html>
