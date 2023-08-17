cce51fb98fb6f5f112a1fd0fcd706051aee5f19c
Sarah Hartmann <sarahsegieth@gmail.com>
2023-08-17 21:27:45 +0200

    add Adrianas patterns

--- src/main/resources/db/changelog/002_pattern.sql
+++ src/main/resources/db/changelog/002_pattern.sql
@@ -1,5 +1,11 @@
 create table pattern (
 id serial primary key,
 name varchar,
-publisher varchar
+publisher varchar,
+published_in varchar,
+rating varchar,
+difficulty float,
+img_url varchar,
+fabric_consumption_metric float,
+fabric_consumption_imperial float
 )
