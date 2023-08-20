d2d1f2f074b76c3515c97b5a24c5d669de7dc39e
Sarah Hartmann <sarahsegieth@gmail.com>
2023-08-20 11:59:48 +0200

    change to less chaotic empty project picture

--- src/main/resources/db/changelog/002_pattern.sql
+++ src/main/resources/db/changelog/002_pattern.sql
@@ -5,7 +5,7 @@ publisher varchar,
 published_in varchar,
 rating varchar,
 difficulty float,
-img_url varchar DEFAULT 'https://drive.google.com/file/d/1ImSFvCqlPdk4naYRrMP49NnSDX5-Swd_/preview',
+img_url varchar DEFAULT 'https://drive.google.com/file/d/1MIcOLOdOOQJ4pQ9eBG_yhuatQzXzG98R/preview',
 fabric_consumption_metric float,
 fabric_consumption_imperial float
 )
