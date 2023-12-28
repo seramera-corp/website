e3a42d2babff392574765298b175bdb2cd29abbb
Sarah Hartmann <sarahsegieth@gmail.com>
2023-12-28 09:13:47 +0100

    changes to some simple style elements and exchanges default picture for empty pattern with neutral image

--- src/main/resources/db/changelog/002_pattern.sql
+++ src/main/resources/db/changelog/002_pattern.sql
@@ -6,6 +6,6 @@ published_in varchar,
 publishing_date date,
 difficulty varchar,
 community_rating_difficulty float,
-img_url varchar DEFAULT 'https://drive.google.com/file/d/1MIcOLOdOOQJ4pQ9eBG_yhuatQzXzG98R/preview',
+img_url varchar DEFAULT 'https://drive.google.com/file/d/1O9UV5WgxYKPbIUDqQcggTwLS4t6eNj8p/preview',
 community_rating float
 )
